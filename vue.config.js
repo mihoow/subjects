cb5b117221c419a57e4da48029d0a78370291715
Michał Wieczorek <michal.wieczorek@scandiweb.com>
2024-12-26 00:18:47 +0100

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/subjects/' : '/',
+  publicPath: '/',
   chainWebpack: (config) => {
     config.module.rule('markdown').test(/\.md$/).use('raw-loader').loader('raw-loader').end();
   },
